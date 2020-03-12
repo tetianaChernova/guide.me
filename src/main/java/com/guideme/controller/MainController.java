f9fbd5f67c1099439992fc0dc48903d632b73bc0
tetiana.chernova <chtania.2000@gmail.com>
2020-03-12 14:32:28 +0200

    Added excursion model, general refactoring

--- src/main/java/com/guideme/controller/MainController.java
+++ src/main/java/com/guideme/controller/MainController.java
@@ -17,7 +17,7 @@ public class MainController {
 
 	@GetMapping("/")
 	public String greeting(Model model) {
-		return "landingPade";
+		return "greeting";
 	}
 
 	@GetMapping("/main")
---
+++ src/main/java/com/guideme/model/Excursion.java
@@ -0,0 +1,56 @@
+package com.guideme.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Excursion {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long excursionId;
+	@Column(nullable = false)
+	private String title;
+	@Column(nullable = false)
+	private String city;
+	@Column(nullable = false)
+	private Integer duration;
+	@Column(nullable = false)
+	private Integer priceForOne;
+	@Column(nullable = false)
+	private String description;
+	@Column(nullable = false)
+	private Integer amount;
+
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "excursion_language", joinColumns = @JoinColumn(name = "excursion_id"))
+	private List<String> languages;
+
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "excursion_language", joinColumns = @JoinColumn(name = "excursion_id"))
+	private List<String> sightseeings;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "guide_id")
+	private Guide guide;
+}
